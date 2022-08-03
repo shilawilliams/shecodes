3bde3f2a252b1b8828230c29f03ff7840d1d0ff8
shilawilliams <108036179+shilawilliams@users.noreply.github.com>
2022-08-02 20:02:47 -0500

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,17 @@
+# shebang /bin/bash
+
+# Script Name:touch demo 1.sh
+# Author Name: Shila Williams
+# Date of revision
+# Description of purpose: First Bash Script
+# Example usage:$ demon 1.sh
+# Expected output: " I made a script"
+# Declaration of variables (if any): $str_textline
+# Declaration of functions (if any):
+
+
+#$str_textline="guess i did it"
+
+# Main
+echo $str_textline
+# End
